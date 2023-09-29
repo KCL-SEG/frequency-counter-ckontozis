178cb2f7770bb00e3520747e6b99056bbb2c1d0f
Constantinos Kontozis <constantinos.kontozis@kcl.ac.uk>
2023-09-29 20:40:50 +0100

    Measure frequency of each item in provided list

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,17 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if not isinstance(item, str):
+            key = str(item)
+        else:
+            key = item
+
+        if key in frequencies:
+            frequencies[key] += 1
+        else:
+            frequencies[key] = 1
     return frequencies
